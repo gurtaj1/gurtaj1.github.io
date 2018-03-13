684b4481673b9249ec451397af62d106a064a569
Gurtaj <gurtaj@gmail.com>
2018-03-13 01:52:11 +0000

    Add screenshots for portfolio project images

--- functions.js
+++ functions.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     $(".navToggle").click(function(){
         if ($(".narrowLinks").css("display") === "none") {
-            $(".narrowLinks").slideDown();
+            $(".narrowLinks").slideDown(function(){
+                $(".narrowLinks").css({"border-top": "1px solid rgb(80, 0, 80)"});
+            });
         } else {
+            $(".narrowLinks").css({"border-top": "none"});
             $(".narrowLinks").slideUp();
         }
     })
