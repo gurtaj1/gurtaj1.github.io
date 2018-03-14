566029d70c37212189266515ca5e790239ecf46e
Gurtaj <gurtaj@gmail.com>
2018-03-14 03:42:37 +0000

    Add .linkPage for each project displayed. In each 'page' (a div that appears and the rest of the page disappears - using jQuery) add a link to repository and actual project github page, as well as a description of the project. Slightly update style.

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //navbar functions
     $(".navToggle").click(function(){
         if ($(".narrowLinks").css("display") === "none") {
             $(".narrowLinks").slideDown(function(){
@@ -14,4 +15,29 @@ $(document).ready(function(){
             $(".narrowLinks").css({"display": "none"});
         } 
     })
+    //linkPage unhide/hide functions
+    var linkPageId;
+    var scrollPosition1;
+
+    $(".screen").click(function(event){
+        linkPageId = "#" + event.target.id + "Page";
+        scrollPosition1 = $(document).scrollTop();
+        $(linkPageId).css({"top": scrollPosition1})
+        $(linkPageId).fadeIn(function(){
+            $(linkPageId).css({"top": "0px"})
+            $(".normalView").css({"display": "none"});
+            window.scrollTo(0, 0);
+        });
+    })
+
+    $(".closeLinkView").click(function(){
+        var scrollPosition2 = $(document).scrollTop();
+        var scrollPositionDifference = scrollPosition1 - scrollPosition2;
+        $(".normalView").css({"display": "block"});
+        window.scrollTo(0, scrollPosition1);
+        $(linkPageId).css({"top": scrollPositionDifference})
+        $(linkPageId).fadeOut();
+
+    })
+
 })
